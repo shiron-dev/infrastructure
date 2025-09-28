7fb2e1a6c9f7ab60955ad9c83aca03f127fa2269
shiron <shiron4710.dev@gmail.com>
2025-09-29 01:37:57 +0900

    wip

--- terraform/kms.tf
+++ terraform/kms.tf
@@ -9,16 +9,21 @@ resource "google_kms_crypto_key" "sops_key" {
   name     = "sops-key"
   key_ring = google_kms_key_ring.sops.id
   purpose  = "ENCRYPT_DECRYPT"
+  
+  rotation_period = "7776000s"  # 90 days in seconds
 }
 
 output "kms_keyring_name" {
+  description = "Name of the KMS key ring for SOPS encryption"
   value = google_kms_key_ring.sops.name
 }
 
 output "kms_key_name" {
+  description = "Name of the KMS crypto key for SOPS encryption"
   value = google_kms_crypto_key.sops_key.name
 }
 
 output "kms_key_id" {
+  description = "ID of the KMS crypto key for SOPS encryption"
   value = google_kms_crypto_key.sops_key.id
 }
