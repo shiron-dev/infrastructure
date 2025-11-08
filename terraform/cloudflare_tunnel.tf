69f33cfc2130dde851ceddb4fa69aad778191fb8
shiron <shiron4710.dev@gmail.com>
2025-11-08 17:37:11 +0900

    wip

--- terraform/cloudflare_tunnel.tf
+++ terraform/cloudflare_tunnel.tf
@@ -3,22 +3,6 @@ resource "cloudflare_zero_trust_tunnel_cloudflared" "arm_service_honor" {
   name       = "arm-service-honor"
 }
 
-resource "cloudflare_zero_trust_tunnel_cloudflared_config" "arm_service_honor_config" {
-  account_id = local.cloudflare_account_id
-  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.arm_service_honor.id
-
-  config = {
-    ingress = [
-      {
-        hostname = "services.shiron.dev"
-        service  = "http://homer:8080"
-      },
-      {
-        service = "http_status:404"
-      }
-    ]
-  }
-}
 
 resource "cloudflare_dns_record" "arm_service_honor_cname" {
   zone_id = local.cloudflare_shiron_dev_zone_id
@@ -27,4 +11,9 @@ resource "cloudflare_dns_record" "arm_service_honor_cname" {
   content = "${cloudflare_zero_trust_tunnel_cloudflared.arm_service_honor.id}.cfargotunnel.com"
   proxied = true
   ttl     = 1
+}
+
+output "arm_service_honor_tunnel_token" {
+  value = cloudflare_zero_trust_tunnel_cloudflared.arm_service_honor.id
+  sensitive = true
 }
