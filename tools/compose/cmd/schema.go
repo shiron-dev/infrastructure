1e6834d9edb262bfc93de4753c0d99072149df37
shiron <shiron4710.dev@gmail.com>
2026-02-16 02:54:53 +0900

    refactor(schema): move schema generation and tests to config package for better organization

--- tools/compose/cmd/schema.go
+++ tools/compose/cmd/schema.go
@@ -1,13 +1,9 @@
 package cmd
 
 import (
-	"encoding/json"
-	"fmt"
+	"cmt/internal/config"
 	"os"
 
-	"cmt/internal/config"
-
-	"github.com/invopop/jsonschema"
 	"github.com/spf13/cobra"
 )
 
@@ -18,7 +14,7 @@ func newSchemaCmd() *cobra.Command {
 	schemaCommand.Args = cobra.ExactArgs(1)
 	schemaCommand.ValidArgs = []string{"cmt", "host"}
 	schemaCommand.RunE = func(_ *cobra.Command, args []string) error {
-		data, err := generateSchemaJSON(args[0])
+		data, err := config.GenerateSchemaJSON(args[0])
 		if err != nil {
 			return err
 		}
@@ -29,37 +25,4 @@ func newSchemaCmd() *cobra.Command {
 	}
 
 	return schemaCommand
-}
-
-// generateSchemaJSON returns the JSON Schema bytes for the given schema type.
-func generateSchemaJSON(kind string) ([]byte, error) {
-	var target any
-
-	switch kind {
-	case "cmt":
-		targetConfig := new(config.CmtConfig)
-		targetConfig.BasePath = ""
-		targetConfig.Defaults = nil
-		targetConfig.Hosts = nil
-		target = targetConfig
-	case "host":
-		targetHostConfig := new(config.HostConfig)
-		targetHostConfig.SSHConfig = ""
-		targetHostConfig.RemotePath = ""
-		targetHostConfig.PostSyncCommand = ""
-		targetHostConfig.Projects = nil
-		target = targetHostConfig
-	default:
-		return nil, fmt.Errorf("unknown schema type %q (use \"cmt\" or \"host\")", kind)
-	}
-
-	r := new(jsonschema.Reflector)
-	schema := r.Reflect(target)
-
-	data, err := json.MarshalIndent(schema, "", "  ")
-	if err != nil {
-		return nil, fmt.Errorf("marshalling schema: %w", err)
-	}
-
-	return data, nil
 }
---
+++ tools/compose/internal/config/schema.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/invopop/jsonschema"
+)
+
+func GenerateSchemaJSON(kind string) ([]byte, error) {
+	var target any
+
+	switch kind {
+	case "cmt":
+		targetConfig := new(CmtConfig)
+		targetConfig.BasePath = ""
+		targetConfig.Defaults = nil
+		targetConfig.Hosts = nil
+		target = targetConfig
+	case "host":
+		targetHostConfig := new(HostConfig)
+		targetHostConfig.SSHConfig = ""
+		targetHostConfig.RemotePath = ""
+		targetHostConfig.PostSyncCommand = ""
+		targetHostConfig.Projects = nil
+		target = targetHostConfig
+	default:
+		return nil, fmt.Errorf("unknown schema type %q (use \"cmt\" or \"host\")", kind)
+	}
+
+	r := new(jsonschema.Reflector)
+	schema := r.Reflect(target)
+
+	data, err := json.MarshalIndent(schema, "", "  ")
+	if err != nil {
+		return nil, fmt.Errorf("marshalling schema: %w", err)
+	}
+
+	return data, nil
+}
