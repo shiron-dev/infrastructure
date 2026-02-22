dc954b624833f2c9037627e4ea89381e2af95904
shiron <shiron4710.dev@gmail.com>
2026-02-22 16:20:21 +0900

    refactor(terraform): consolidate API service definitions and update dependencies

--- terraform/api.tf
+++ terraform/api.tf
@@ -1,3 +1,11 @@
-resource "google_project_service" "cloudkms_api" {
-  service = "cloudkms.googleapis.com"
+locals {
+  api_cloudkms         = "cloudkms.googleapis.com"
+  api_iamcredentials   = "iamcredentials.googleapis.com"
+  api_sts              = "sts.googleapis.com"
+  project_services     = toset([local.api_cloudkms, local.api_iamcredentials, local.api_sts])
+}
+
+resource "google_project_service" "apis" {
+  for_each = local.project_services
+  service  = each.key
 }
